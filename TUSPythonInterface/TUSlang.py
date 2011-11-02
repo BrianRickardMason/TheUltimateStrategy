d4508fe4d336eb1be17b066f0c598dd218719e12
Brian Rickard Mason <brian.rickard.mason@gmail.com>
2011-11-02 18:33:27 +0100

    A new format of TUSlang proposed.

--- TUSPythonInterface/TUSlang.py
+++ TUSPythonInterface/TUSlang.py
@@ -5,21 +5,25 @@
 #Full list of avaliable commands with their identifiers as well as the number, type and name of required fields for each XML request (command)
 #TODO: Handle default 'status' : '' (only params in dict)
 #TODO: Handle login
+#TODO: Request ids have to be consistent with the ones that are held in Request.hpp file.
+
+# Command: ( REQUEST_ID, {<user></user>}, {<parameters></parameters>} ).
 commands = {
-'ECHO' : (1, {}, {}),
-'ERROR' : (2, {}, {}),
+# Generic.
+'ECHO'              : ( 1, {}, {}),
+'ERROR'             : ( 2, {}, {}),
 
-'CREATE_LAND' : (3, {'idworld': 'unsigned integer', 'idepoch': 'unsigned integer', 'name': 'string'}, {'message': 'string'}),
-'DELETE_LAND' : (4, {'idland': 'unsigned integer'}, {'message': 'string'}),
-'GET_LAND_BY_ID_LAND' : (5, {'idland': 'unsigned integer'}, {'message': 'string', 'iduser': 'unsigned integer', 'idworld': 'unsigned integer', 'idland': 'unsigned integer', 'name': 'string', 'granted': 'boolean'}),
-'GET_LAND_BY_NAME' : (6, {'name': 'string'}, {'idland': 'unsigned integer'}, {'message': 'string', 'iduser': 'unsigned integer', 'idworld': 'unsigned integer', 'idland': 'unsigned integer', 'name': 'string', 'granted': 'boolean'}),
-#'GET_LANDS_BY_ID_WORLD' : (7, {}, {}), - not used in this version of the language
+# Land.
+'CREATE_LAND'       : ( 3, {'login', 'password'}, {'world_name', 'land_name'}),
+'DELETE_LAND'       : ( 4, {'login', 'password'}, {'land_name'}),
+'GET_LAND'          : ( 5, {'login', 'password'}, {'land_name'}),
+'GET_LANDS'         : ( 6, {'login', 'password'}, {}),
 
-'CREATE_SETTLEMENT' : (8, {'idland': 'unsigned integer', 'name': 'string'}, {'message': 'string'}),
-'DELETE_SETTLEMENT' : (9, {}, {}),
-'GET_SETTLEMENT_BY_ID_LAND_AND_NAME' : (10, {}, {}),
-'GET_SETTLEMENT_BY_ID_SETTLEMENT' : (11, {}, {}),
-'GET_SETTLEMENTS_BY_ID_LAND' : (12, {}, {}),
+# Settlement.
+'CREATE_SETTLEMENT' : ( 7, {'login', 'password'}, {'land_name', 'settlement_name'}),
+'DELETE_SETTLEMENT' : ( 8, {'login', 'password'}, {'settlement_name'}),
+'GET_SETTLEMENT'    : ( 9, {'login', 'password'}, {'settlement_name'}),
+'GET_SETTLEMENTS'   : (10, {'login', 'password'}, {'land_name'}),
 
 'BUILD_BUILDING' : (13, {}, {}),
 'DESTROY_BUILDING' : (14, {}, {}),
@@ -52,6 +56,7 @@ commands = {
 'TRANSPORT_RESOURCE' : (34, {}, {}),
 }
 
+#TODO: FIXME
 status = {
 1 : 'REPLY_STATUS_SERVER_IS_NOT_LISTENING',
 2 : 'REPLY_STATUS_INVALID_REQUEST',
