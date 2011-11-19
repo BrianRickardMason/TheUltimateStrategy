60aff57678bfd3f6de493875bbe8dcddaa28cf7e
bkramarski <b.kramarski@gmail.com>
2011-11-20 00:41:43 +0100

    TUSConfiguration with initial version of viewing build costs.

--- TUSPythonInterface/TUSConfiguration.py
+++ TUSPythonInterface/TUSConfiguration.py
@@ -54,19 +54,44 @@ def getPropertiesAsMap(aFileName, aObjectTagName):
         oDict[k] = props
     return oDict
 
-def getObjectsWithProperties(aObjectName, aFileName, aPropFileName):
+def getCostAsMap(aFileName, aObjectTagName):
+    """Extracts cost for given object action from xml file"""
+    root = parse(aFileName)
+    normalizeTotally(root)
+    
+    obCostDict = {}
+    obList = root.getElementsByTagName(aObjectTagName)
+    for ob in obList:
+        k = ob.getAttribute('key')
+        obCostDict[k] = {}
+        for act in ob.childNodes :
+            cost = {}
+            if act :
+                for r in act.childNodes :
+                    cost[r.tagName] = r.getAttribute('value')
+            obCostDict[k][act.tagName] = cost
+    return obCostDict
+    
+def getObjectsWithProperties(aObjectName, aFileName, aPropFileName='',aCostFileName=''):
     """Extracts objects and properties, and combines them in one map"""
     oDict = getObjectsAsMap(aFileName, aObjectName)
-    pDict = getPropertiesAsMap(aPropFileName, aObjectName)
 
-    for i, ob in pDict.items():
-        for p, v in ob.items():
-            oDict[i][p]=v
+    if aPropFileName :
+        pDict = getPropertiesAsMap(aPropFileName, aObjectName)
+        for i, ob in pDict.items():
+            for p, v in ob.items():
+                oDict[i][p]=v
+    if aCostFileName :
+        cDict = getCostAsMap(aCostFileName, aObjectName)
+        for i, ob in cDict.items():
+            oDict[i]['costs'] = {}
+            for c, v in ob.items():
+                oDict[i]['costs'][c] = v
     return oDict
-    
-def prepareBuildings(aFileName, aPropFileName):
+
+def prepareBuildings(aFileName, aPropFileName, aCostFileName):
     """Extracts buildings and its properties from given files"""
-    return getObjectsWithProperties('building', aFileName, aPropFileName)
+    return getObjectsWithProperties('building', aFileName, aPropFileName, aCostFileName)
  
 def prepareHumans(aFileName, aPropFileName):
     """Extracts humans from given file"""
@@ -89,21 +114,36 @@ def printPossibleHumans(aHumansDict, aEngageableOnly = True):
     """Prints engageable humans' definitions from given file in one line format."""
     # humans.human.{key|class|name|experience} 
     print( "{:>10}{:>16}{:>9}{:>6}{:>6}#".format('class', 'name', 'exp.', 'dism.','prod.'))
-    print( "{:->10}{:->16}{:>9}{:->6}{:->6}#".format('', '', '', '',''))
+    print( "{:->10}{:->16}{:->9}{:->6}{:->6}#".format('', '', '', '',''))
     for i, h in aHumansDict.items():
         if aEngageableOnly and h['engageable'] != 'true':
             continue
         print( "{:>10}{:>16}{:>9}{:>6}{:>6}".format(
             h['class'], h['name'], h['experience'], h['dismissable'], h['production']))
 
+# TODO consider different set of resources            
+def costString(cost):
+    """Returns cost strings for presenting in one line."""
+    fullCost = {'food':0,'gold':0,'wood':0,'iron':0,'rock':0,'mana':0,'coal':0}
+    for c,v in cost.items():
+        fullCost[c] = v
+    return "{food:>3}{gold:>3}{wood:>3}{iron:>3}{rock:>3}{mana:>3}{coal:>3}".format(**fullCost)
+
+def costHead():
+    """Returns cost header string."""
+    fullCost = {'food':0,'gold':0,'wood':0,'iron':0,'rock':0,'mana':0,'coal':0}
+    for c,v in fullCost.items():
+        fullCost[c] = c[0]
+    return "{food:>3}{gold:>3}{wood:>3}{iron:>3}{rock:>3}{mana:>3}{coal:>3}".format(**fullCost)
+    
 def printPossibleBuildings(aBuildingsDict):
     """Prints buildings' definitions from given files in one line format."""
     # buildings.building.{key|class|name} 
-    print( "{0:>10}{1:>20}{2:>6}#".format('class', 'name', 'cap.'))
-    print( "{0:->10}{1:->20}{2:->6}#".format('', '', ''))
+    print( "{0:>10}{1:>20}{2:>6} b:{3:->21}#".format('class', 'name', 'cap.', costHead()))
+    print( "{0:->10}{1:->20}{2:->6}---{3:->21}#".format('', '', '', ''))
     for i, b in aBuildingsDict.items():
-        print( "{0:>10}{1:>20}{2:>6}".format(
-            b['class'], b['name'], b['capacity']))
+        print( "{0:>10}{1:>20}{2:>6} b:{3}".format(
+            b['class'], b['name'], b['capacity'], costString(b['costs']['build']) ))
 
 
 # Handling usage as script
@@ -127,10 +167,11 @@ def main(argv=None):
     hpFile = dataRoot + '/Human/properties.xml'
     bFile =  dataRoot + '/Building/buildings.xml'
     bpFile = dataRoot + '/Building/properties.xml'
+    bcFile = dataRoot + '/Building/costs.xml'
     rFile =  dataRoot + '/Resource/resources.xml'
     
     humans = prepareHumans(hFile, hpFile) 
-    buildings = prepareBuildings(bFile, bpFile) 
+    buildings = prepareBuildings(bFile, bpFile, bcFile) 
     resources = prepareResources(rFile) 
     
     if type == 'humans' :
