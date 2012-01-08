fddfd66a51f1bd31a8eaa32ff2cd0bd7dfa1e917
Brian Rickard Mason <brian.rickard.mason@gmail.com>
2012-01-08 13:55:29 +0100

    LanguageToProtocolTranslator implements translation of the requests.

---
+++ Protocol/Xml/Cpp/LanguageToProtocolTranslator.cpp
@@ -0,0 +1,285 @@
+// Copyright (C) 2010, 2011 and 2012 Marcin Arkadiusz Skrobiranda.
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions
+// are met:
+// 1. Redistributions of source code must retain the above copyright
+//    notice, this list of conditions and the following disclaimer.
+// 2. Redistributions in binary form must reproduce the above copyright
+//    notice, this list of conditions and the following disclaimer in the
+//    documentation and/or other materials provided with the distribution.
+// 3. Neither the name of the project nor the names of its contributors
+//    may be used to endorse or promote products derived from this software
+//    without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE PROJECT AND CONTRIBUTORS "AS IS" AND
+// ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE PROJECT OR CONTRIBUTORS BE LIABLE
+// FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+// DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+// OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+// HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+// LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+// OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+// SUCH DAMAGE.
+
+#include <Protocol/Xml/Cpp/LanguageToProtocolTranslator.hpp>
+#include <Protocol/Xml/Cpp/MessageFactory.hpp>
+#include <boost/assert.hpp>
+
+namespace TUSProtocol
+{
+
+Message::SingleHandle LanguageToProtocolTranslator::translate(
+    TUSLanguage::ICommand::SingleHandle a_command
+) const
+{
+    MessageFactory message_factory;
+
+    switch (a_command->getID())
+    {
+        case TUSLanguage::ID_COMMAND_ECHO_REQUEST:
+            return message_factory.createEchoRequest();
+
+        case TUSLanguage::ID_COMMAND_ERROR_REQUEST:
+            return message_factory.createErrorRequest();
+
+        case TUSLanguage::ID_COMMAND_CREATE_LAND_REQUEST:
+            return message_factory.createCreateLandRequest(
+                       a_command->getLogin(),
+                       a_command->getPassword(),
+                       a_command->getParam("world_name"),
+                       a_command->getParam("land_name")
+                   );
+
+        case TUSLanguage::ID_COMMAND_DELETE_LAND_REQUEST:
+            return message_factory.createDeleteLandRequest(
+                       a_command->getLogin(),
+                       a_command->getPassword(),
+                       a_command->getParam("land_name")
+                   );
+
+        case TUSLanguage::ID_COMMAND_GET_LAND_REQUEST:
+            return message_factory.createGetLandRequest(
+                       a_command->getLogin(),
+                       a_command->getPassword(),
+                       a_command->getParam("land_name")
+                   );
+
+        case TUSLanguage::ID_COMMAND_GET_LANDS_REQUEST:
+            return message_factory.createGetLandsRequest(
+                       a_command->getLogin(),
+                       a_command->getPassword()
+                   );
+
+        case TUSLanguage::ID_COMMAND_CREATE_SETTLEMENT_REQUEST:
+            return message_factory.createCreateSettlementRequest(
+                       a_command->getLogin(),
+                       a_command->getPassword(),
+                       a_command->getParam("land_name"),
+                       a_command->getParam("settlement_name")
+                   );
+
+        case TUSLanguage::ID_COMMAND_DELETE_SETTLEMENT_REQUEST:
+            return message_factory.createDeleteSettlementRequest(
+                       a_command->getLogin(),
+                       a_command->getPassword(),
+                       a_command->getParam("settlement_name")
+                   );
+
+        case TUSLanguage::ID_COMMAND_GET_SETTLEMENT_REQUEST:
+            return message_factory.createGetSettlementRequest(
+                       a_command->getLogin(),
+                       a_command->getPassword(),
+                       a_command->getParam("settlement_name")
+                   );
+
+        case TUSLanguage::ID_COMMAND_GET_SETTLEMENTS_REQUEST:
+            return message_factory.createGetSettlementsRequest(
+                       a_command->getLogin(),
+                       a_command->getPassword(),
+                       a_command->getParam("land_name")
+                   );
+
+        case TUSLanguage::ID_COMMAND_BUILD_BUILDING_REQUEST:
+            return message_factory.createBuildBuildingRequest(
+                       a_command->getLogin(),
+                       a_command->getPassword(),
+                       a_command->getParam("idholderclass"),
+                       a_command->getParam("holder_name"),
+                       a_command->getParam("buildingkey"),
+                       a_command->getParam("volume")
+                   );
+
+        case TUSLanguage::ID_COMMAND_DESTROY_BUILDING_REQUEST:
+            return message_factory.createDestroyBuildingRequest(
+                       a_command->getLogin(),
+                       a_command->getPassword(),
+                       a_command->getParam("idholderclass"),
+                       a_command->getParam("holder_name"),
+                       a_command->getParam("buildingkey"),
+                       a_command->getParam("volume")
+                   );
+
+        case TUSLanguage::ID_COMMAND_GET_BUILDING_REQUEST:
+            return message_factory.createGetBuildingRequest(
+                       a_command->getLogin(),
+                       a_command->getPassword(),
+                       a_command->getParam("idholderclass"),
+                       a_command->getParam("holder_name"),
+                       a_command->getParam("buildingkey")
+                   );
+
+        case TUSLanguage::ID_COMMAND_GET_BUILDINGS_REQUEST:
+            return message_factory.createGetBuildingsRequest(
+                       a_command->getLogin(),
+                       a_command->getPassword(),
+                       a_command->getParam("idholderclass"),
+                       a_command->getParam("holder_name")
+                   );
+
+        case TUSLanguage::ID_COMMAND_DISMISS_HUMAN_REQUEST:
+            return message_factory.createDismissHumanRequest(
+                       a_command->getLogin(),
+                       a_command->getPassword(),
+                       a_command->getParam("idholderclass"),
+                       a_command->getParam("holder_name"),
+                       a_command->getParam("humankey"),
+                       a_command->getParam("volume")
+                   );
+
+        case TUSLanguage::ID_COMMAND_ENGAGE_HUMAN_REQUEST:
+            return message_factory.createEngageHumanRequest(
+                       a_command->getLogin(),
+                       a_command->getPassword(),
+                       a_command->getParam("idholderclass"),
+                       a_command->getParam("holder_name"),
+                       a_command->getParam("humankey"),
+                       a_command->getParam("volume")
+                   );
+
+        case TUSLanguage::ID_COMMAND_GET_HUMAN_REQUEST:
+            return message_factory.createGetHumanRequest(
+                       a_command->getLogin(),
+                       a_command->getPassword(),
+                       a_command->getParam("idholderclass"),
+                       a_command->getParam("holder_name"),
+                       a_command->getParam("humankey")
+                   );
+
+        case TUSLanguage::ID_COMMAND_GET_HUMANS_REQUEST:
+            return message_factory.createGetHumansRequest(
+                       a_command->getLogin(),
+                       a_command->getPassword(),
+                       a_command->getParam("idholderclass"),
+                       a_command->getParam("holder_name")
+                   );
+
+        case TUSLanguage::ID_COMMAND_GET_RESOURCE_REQUEST:
+            return message_factory.createGetResourceRequest(
+                       a_command->getLogin(),
+                       a_command->getPassword(),
+                       a_command->getParam("idholderclass"),
+                       a_command->getParam("holder_name"),
+                       a_command->getParam("resourcekey")
+                   );
+
+        case TUSLanguage::ID_COMMAND_GET_RESOURCES_REQUEST:
+            return message_factory.createGetResourcesRequest(
+                       a_command->getLogin(),
+                       a_command->getPassword(),
+                       a_command->getParam("idholderclass"),
+                       a_command->getParam("holder_name")
+                   );
+
+        case TUSLanguage::ID_COMMAND_CREATE_USER_REQUEST:
+            return message_factory.createCreateUserRequest(
+                       a_command->getParam("login"),
+                       a_command->getParam("password")
+                   );
+
+        case TUSLanguage::ID_COMMAND_CREATE_WORLD_REQUEST:
+            return message_factory.createCreateWorldRequest(
+                       a_command->getLogin(),
+                       a_command->getPassword(),
+                       a_command->getParam("world_name")
+                   );
+
+        case TUSLanguage::ID_COMMAND_CREATE_EPOCH_REQUEST:
+            return message_factory.createCreateEpochRequest(
+                       a_command->getLogin(),
+                       a_command->getPassword(),
+                       a_command->getParam("world_name"),
+                       a_command->getParam("epoch_name")
+                   );
+
+        case TUSLanguage::ID_COMMAND_DELETE_EPOCH_REQUEST:
+            return message_factory.createDeleteEpochRequest(
+                       a_command->getLogin(),
+                       a_command->getPassword(),
+                       a_command->getParam("world_name")
+                   );
+
+        case TUSLanguage::ID_COMMAND_ACTIVATE_EPOCH_REQUEST:
+            return message_factory.createActivateEpochRequest(
+                       a_command->getLogin(),
+                       a_command->getPassword(),
+                       a_command->getParam("world_name")
+                   );
+
+        case TUSLanguage::ID_COMMAND_DEACTIVATE_EPOCH_REQUEST:
+            return message_factory.createDeactivateEpochRequest(
+                       a_command->getLogin(),
+                       a_command->getPassword(),
+                       a_command->getParam("world_name")
+                   );
+
+        case TUSLanguage::ID_COMMAND_FINISH_EPOCH_REQUEST:
+            return message_factory.createFinishEpochRequest(
+                       a_command->getLogin(),
+                       a_command->getPassword(),
+                       a_command->getParam("world_name")
+                   );
+
+        case TUSLanguage::ID_COMMAND_TICK_EPOCH_REQUEST:
+            return message_factory.createTickEpochRequest(
+                       a_command->getLogin(),
+                       a_command->getPassword(),
+                       a_command->getParam("world_name")
+                   );
+
+        case TUSLanguage::ID_COMMAND_GET_EPOCH_REQUEST:
+            return message_factory.createGetEpochRequest(
+                       a_command->getLogin(),
+                       a_command->getPassword(),
+                       a_command->getParam("world_name")
+                   );
+
+        case TUSLanguage::ID_COMMAND_TRANSPORT_HUMAN_REQUEST:
+            return message_factory.createTransportHumanRequest(
+                       a_command->getLogin(),
+                       a_command->getPassword(),
+                       a_command->getParam("settlement_name_source"),
+                       a_command->getParam("settlement_name_destination"),
+                       a_command->getParam("humankey"),
+                       a_command->getParam("volume")
+                   );
+
+        case TUSLanguage::ID_COMMAND_TRANSPORT_RESOURCE_REQUEST:
+            return message_factory.createTransportResourceRequest(
+                       a_command->getLogin(),
+                       a_command->getPassword(),
+                       a_command->getParam("settlement_name_source"),
+                       a_command->getParam("settlement_name_destination"),
+                       a_command->getParam("resourcekey"),
+                       a_command->getParam("volume")
+                   );
+
+        default:
+            BOOST_ASSERT_MSG(false, "Invalid command ID.");
+    }
+}
+
+} // namespace TUSProtocol
