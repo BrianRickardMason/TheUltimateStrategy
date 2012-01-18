9c297e7c590f3b5e4f60cdee82c4c3ebb9251069
Brian Rickard Mason <brian.rickard.mason@gmail.com>
2012-01-18 16:22:01 +0100

    Protocol::Payload implemented.

---
+++ Protocol/Xml/Cpp/Payload.cpp
@@ -0,0 +1,60 @@
+// Copyright (C) 2010, 2011 and 2012 Marcin Arkadiusz Skrobiranda.
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions
+// are met:
+// 1. Redistributions of source code must retain the above copyright
+//    notice, this list of conditions and the following disclaimer.
+// 2. Redistributions in binary form must reproduce the above copyright
+//    notice, this list of conditions and the following disclaimer in the
+//    documentation and/or other materials provided with the distribution.
+// 3. Neither the name of the project nor the names of its contributors
+//    may be used to endorse or promote products derived from this software
+//    without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE PROJECT AND CONTRIBUTORS "AS IS" AND
+// ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE PROJECT OR CONTRIBUTORS BE LIABLE
+// FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+// DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+// OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+// HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+// LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+// OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+// SUCH DAMAGE.
+
+#include <Poco/DOM/DOMWriter.h>
+#include <Poco/XML/XMLWriter.h>
+#include <Protocol/Xml/Cpp/Payload.hpp>
+#include <sstream>
+
+namespace TUSProtocol
+{
+
+Payload::Payload(
+    Message::Handle aMessage
+)
+{
+    Poco::XML::DOMWriter writer;
+    writer.setOptions(Poco::XML::XMLWriter::CANONICAL);
+
+    std::stringstream sstream;
+    writer.writeNode(sstream, aMessage.get());
+
+    mContent = sstream.str();
+    mLength = mContent.length();
+}
+
+int Payload::getLength() const
+{
+    return mLength;
+}
+
+std::string const & Payload::getContent() const
+{
+    return mContent;
+}
+
+} // namespace TUSProtocol
